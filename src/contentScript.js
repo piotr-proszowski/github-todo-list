bc32fb110e8ff7c44d9fe8294d28b421a60f01bc
piotr-proszowski <piotr.proszowski@allegro.pl>
2022-11-19 18:37:00 +0100

    Simplify - remove background

--- src/contentScript.js
+++ src/contentScript.js
@@ -1,21 +1,22 @@
-const toDoItems = [...document.getElementsByClassName("unminimized-comment")]
-  .filter(comment => comment.getElementsByClassName("task-list-item").length > 0)
-  .map(comment => {
-    const obj = {}
-    obj['url'] = `${comment.baseURI}#${comment.id}`
-    obj['content'] = `${comment.getElementsByClassName("task-list-item")[0].innerText}`
-    return obj
-  });
+function getToDoItems() {
+  const toDoItems = [...document.getElementsByClassName("unminimized-comment")]
+    .filter(comment => comment.getElementsByClassName("task-list-item").length > 0)
+    .filter(comment => comment.querySelector("input:checked") == null)
+    .map(comment => {
+      const baseUri = comment.baseURI
+      const indexOfHash = baseUri.indexOf("#")
+      const newUri = baseUri.substring(0, indexOfHash)
+      const obj = {};
+      obj['url'] = `${newUri}#${comment.id}`;
+      obj['content'] = `${comment.getElementsByClassName("task-list-item")[0].innerText}`;
+      return obj;
+    });
+  return toDoItems
+}
 
-chrome.runtime.sendMessage(
-  {
-    type: 'SAVE_TO_DO_ITEMS',
-    payload: {
-      toDoItems: toDoItems,
-    },
-  },
-  (response) => {
-    console.log(toDoItems)
-    console.log(response)
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if(request.type === 'LOAD_TO_DO_ITEMS') {
+    const toDoItems = getToDoItems()
+    sendResponse({items: toDoItems})
   }
-);
+});
--- src/popup.js
+++ src/popup.js
@@ -12,28 +12,36 @@
     );
   }
 
-  chrome.runtime.sendMessage(
-    {
-      type: 'FETCH_TO_DO_LIST',
-      payload: {},
-    },
-    (response) => {
-      const toDoList = document.getElementById("todo-list");
-      const items = response.items;
-      items.forEach(item => {
-        const listItem = document.createElement("li");
-        const checkbox = document.createElement("input");
-        checkbox.type = "checkbox"
-        checkbox.name = item.content
-        listItem.appendChild(checkbox);
-        const link = document.createElement("a");
-        link.innerText = item.content
-        link.setAttribute("href", item.url)
-        listItem.appendChild(link);
-        toDoList.appendChild(listItem);
+  async function getCurrentTab() {
+    let queryOptions = { active: true, lastFocusedWindow: true };
+    // `tab` will either be a `tabs.Tab` instance or `undefined`.
+    let [tab] = await chrome.tabs.query(queryOptions);
+    return tab;
+  }
 
-        link.addEventListener("click", () => { scrollToComment(item.url)});
-      });
-    }
-  );
+  getCurrentTab()
+    .then((tab) => {
+      chrome.tabs.sendMessage(
+        tab.id,
+        {type: 'LOAD_TO_DO_ITEMS', payload: 'example message from popup.js'},
+        {},
+        (response) => {
+          const toDoList = document.getElementById("todo-list");
+          const items = response.items;
+          items.forEach(item => {
+            const listItem = document.createElement("li");
+            const checkbox = document.createElement("input");
+            checkbox.type = "checkbox"
+            checkbox.name = item.content
+            listItem.appendChild(checkbox);
+            const link = document.createElement("a");
+            link.innerText = item.content
+            link.setAttribute("href", item.url)
+            listItem.appendChild(link);
+            toDoList.appendChild(listItem);
+
+            link.addEventListener("click", () => { scrollToComment(item.url)});
+          });
+        });
+    });
 })();
