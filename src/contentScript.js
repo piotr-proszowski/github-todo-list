fa14799ed59cd24da2027600289e55365022df3b
piotr-proszowski <piotr.proszowski@allegro.pl>
2022-11-20 18:36:35 +0100

    Another ui improvements

--- src/contentScript.js
+++ src/contentScript.js
@@ -2,17 +2,21 @@ function getToDoItems() {
   const toDoItems = [...document.getElementsByClassName("unminimized-comment")]
     .filter(comment => comment.getElementsByClassName("task-list-item").length > 0)
     .filter(comment => comment.querySelector("input:checked") == null)
-    .map(comment => {
+    .flatMap(comment => {
       const baseUri = comment.baseURI
       const indexOfHash = baseUri.indexOf("#")
       let newUri = baseUri
       if(indexOfHash > 0) {
         newUri = baseUri.substring(0, indexOfHash)
       }
-      const obj = {};
-      obj['url'] = `${newUri}#${comment.id}`;
-      obj['content'] = `${comment.getElementsByClassName("task-list-item")[0].innerText}`;
-      return obj;
+
+      return [...comment.getElementsByClassName("task-list-item")]
+        .map((task) => {
+            const obj = {};
+            obj['url'] = `${newUri}#${comment.id}`;
+            obj['content'] = `${task.innerText}`;
+            return obj;
+        });
     });
   return toDoItems
 }
